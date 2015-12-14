1e5f4008625515ee30ecead6776eb5cb9ff025be
Jan Sandbrink <nobodysnightmare@gmail.com>
2015-12-14 19:20:29 +0100

    Use real assets in fleet renderer

--- lib/yaaib/visual_runner/fleet_renderer.rb
+++ lib/yaaib/visual_runner/fleet_renderer.rb
@@ -8,8 +8,20 @@ module YaAIB
         SIZE = 16
 
         def draw(position, z, color, text)
-          Gosu.draw_rect(position.x - (SIZE / 2), position.y - (SIZE / 2), SIZE, SIZE, color)
-          FONT.draw_rel(text, position.x, position.y, 1, 0.5, 0.5)
+          scale = SIZE.to_f / overlay.width
+          tint.draw(position.x - (SIZE / 2), position.y - (SIZE / 2), z, scale, scale, color)
+          overlay.draw(position.x - (SIZE / 2), position.y - (SIZE / 2), z, scale, scale)
+          FONT.draw_rel(text, position.x + (SIZE / 2), position.y + (SIZE / 2), 1, 0, 0)
+        end
+
+        private
+
+        def tint
+          @tint ||= Gosu::Image.new('assets/fleet_tint_texture.png')
+        end
+
+        def overlay
+          @overlay ||= Gosu::Image.new('assets/fleet_overlay_texture.png')
         end
       end
     end
